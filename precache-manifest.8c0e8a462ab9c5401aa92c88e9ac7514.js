b0c373f1b058b626f51aab647215aa6dd8dcf0d2
hoang <vinhyenvodoi98@gmail.com>
2019-07-18 09:47:21 +0700

    Updates

---
+++ precache-manifest.8c0e8a462ab9c5401aa92c88e9ac7514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb04f4c088c8bf9ac223f3691836659",
+    "url": "/react-deploy-githubpages/index.html"
+  },
+  {
+    "revision": "63ecc9b56413d89866db",
+    "url": "/react-deploy-githubpages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-deploy-githubpages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "63ecc9b56413d89866db",
+    "url": "/react-deploy-githubpages/static/js/main.0497c070.chunk.js"
+  },
+  {
+    "revision": "da3bb8de5219fd5c4f8f",
+    "url": "/react-deploy-githubpages/static/js/runtime~main.23471361.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-githubpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
